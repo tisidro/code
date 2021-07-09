389bc73e216e9f84d1572e640167e6e652541ad7
Terri Isidro <terri.isidro@gmail.com>
2021-07-08 21:03:04 -0400

    array practice code

--- Week5/1Array.js
+++ Week5/1Array.js
@@ -10,4 +10,18 @@ console.log(fruits[lastFruit])//works if you put in the variable
 
 console.log(fruits[fruits.length - 1])//still works if you substitute in the value of the variable
 
-console.log(fruits.pop("kiwi"))
+let fruitBowl = ["dragonfruit", "blueberry", "fig"]
+
+console.log(fruitBowl.pop())
+
+console.log(fruitBowl.reverse())
+
+console.log(fruits.reverse())//'banana', 'apple'  (we had removed the last one)
+
+console.log(fruits.slice(1))
+
+console.log(fruits.slice(0, 1))
+
+moreFruit = ["apple", "banana", "cherry", "dragonfruit", "blueberry", "fig"]
+
+console.log(moreFruit.slice(1, 3))//cuts out and prints between 1-3 index
---
+++ Week5/2ArrayCaps.js
@@ -0,0 +1,8 @@
+
+let fruits = ["apple", "banana", "cherry"]
+
+fruit = fruits.reverse()//reverses
+
+fruits.forEach((fruit) => {
+    console.log(fruit.toUpperCase())
+})
