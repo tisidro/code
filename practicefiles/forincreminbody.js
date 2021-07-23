7e3863ed48e04c40b812fdc43794558176d367a6
Terri Isidro <terri.isidro@gmail.com>
2021-07-23 11:59:22 -0400

    incrementor in body, not that you would ever do it in practice, but to show it works!

---
+++ practicefiles/forincreminbody.js
@@ -0,0 +1,22 @@
+
+const myData = [
+    "vermont",
+    "iowa",
+    "ohio",
+    1950,
+    "Terri",
+];
+
+var i = 0;
+
+for (; ;) {
+    if (i > 3) break;
+    console.log(i);
+    i++;
+}
+
+//not suggested in practice but it works! Otherwise just do...
+
+for (i = 0; i < 4; i++) {
+    console.log(i);
+}
