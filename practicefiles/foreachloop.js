33d7906fb864b3182e8a160ad191efd7875e348f
Terri Isidro <terri.isidro@gmail.com>
2021-07-24 13:40:05 -0400

    for each loop code

---
+++ practicefiles/foreachloop.js
@@ -0,0 +1,13 @@
+
+const myData = [
+    "vermont",
+    "iowa",
+    "ohio",
+    1950,
+    "Terri",
+];
+//d is for data in the array
+
+myData.forEach((d) => (console.log(d)));
+
+//advantages: don't need i, no need for initialization, etc. no worries about incrementing/decrementing or length of array
