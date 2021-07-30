812e36925bf19a54e72ff9aa6457033b32353ed7
Terri Isidro <terri.isidro@gmail.com>
2021-07-30 16:31:38 -0400

    adding and changing properties/values in an object.

---
+++ practicefiles/objects/addchangeobj.js
@@ -0,0 +1,18 @@
+//adding and changing properties and values in an object
+//property = key value pair
+//value is the value of a specific key in the object
+
+states = {
+    VT: "Vermont",
+    NY: "New York",
+    OH: "Ohio"
+}
+//add states to object
+states.WY = "Wyoming"
+states.ME = "Maine"
+
+console.log(states)
+
+states.WY = "Just some state"//changes WY value
+
+console.log(states)
