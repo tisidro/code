1038759a3f734fbeb474f5736f631abacfa9f473
Terri Isidro <terri.isidro@gmail.com>
2021-08-16 13:18:45 -0400

    added clarification in description

--- practicefiles/loops/Loopexample.js
+++ practicefiles/loops/Loopexample.js
@@ -7,6 +7,8 @@ for (i = 0; i < links.length; i++) {
 
     links[i].className = "link-" + i;
 
-    //each time you iterate through the loop, give the link a class name
+    //each time you iterate through the loop, give the link a class name based on their link number
+
+    //<a href= "a" class="link-1"> ,etc.
 
 }
