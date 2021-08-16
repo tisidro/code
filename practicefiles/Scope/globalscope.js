dc3524e3c3ed233beba39afa2ed118aa9fbb0cf7
Terri Isidro <terri.isidro@gmail.com>
2021-08-16 14:20:16 -0400

    clarifying explanation at end of file

--- practicefiles/Scope/globalscope.js
+++ practicefiles/Scope/globalscope.js
@@ -1,6 +1,7 @@
 
 //showing how global scope works
 
+
 function getAverage(a, b) {
 
     let average = (a + b) / 2;
@@ -16,6 +17,7 @@ function logResult() {
 
     console.log("the average is " + myResult);
     //can use myResult here b/c it is a GLOBAL variable
+    //if you wanted to use average and log that you could declare let average = 0 at top in the global scope
 
 }
 
