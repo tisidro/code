6d4d2f6d00ed1c9b0e0c69b061af61c1208349dc
Terri Isidro <terri.isidro@gmail.com>
2021-08-22 18:07:37 -0400

    fixed the code. I forgot to put .value after myForm.name but once I did it worked perfectly!:)

--- practicefiles/labs/formvalidsimple/test.js
+++ practicefiles/labs/formvalidsimple/test.js
@@ -1,9 +1,14 @@
+
+//for some reason, it will not give the error message when I click submit!
+//FIXED--I forgot to put ".value" after myForm.name!
+
+
 let myForm = document.forms.myForm;
 let message = document.getElementById("message");
 
 myForm.onsubmit = function () {
 
-    if (myForm.name == "") {
+    if (myForm.name.value == "") {
 
         message.innerHTML = "Please enter a name!";
         return false;
