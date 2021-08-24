5060867abc8e167d60bcaf76ec77618316c0dac8
Terri Isidro <terri.isidro@gmail.com>
2021-08-24 19:03:38 -0400

    Very clear and basic explanation of how you can use classes with constructor functions and the new keyword to easily automate the creation of multiple objects

---
+++ practicefiles/classes/classesintro.js
@@ -0,0 +1,22 @@
+//classes are used with constructor functions and the new keyword to create multiple objects more easily--automates it!
+
+//this creates the class, notice that the name of the class is Capitalized!
+
+class User {
+
+    //constructor function which creates our objects
+
+    constructor(email, name) {
+
+        this.email = email;
+        this.name = name;
+    }
+
+}
+
+let userOne = new User("terri", "terri@email.com");
+
+let userTwo = new User("tina", "tina@email.com");
+
+console.log(userOne);
+console.log(userTwo);
